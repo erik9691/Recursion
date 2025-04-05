6a8c5c9f9310c2ec576be514e0b90fe524be89c2
erik9691 <gagwaffle@gmail.com>
2025-04-05 14:11:39 -0300

    Recursive fibonacci

--- main/index.js
+++ main/index.js
@@ -16,4 +16,21 @@ function fibs(n) {
 	return fibArray;
 }
 
-console.log(fibs(8)); // Should return [0, 1, 1, 2, 3, 5, 8, 13]
+function fibsRec(n) {
+	if (n === 1) {
+		const fibArray = [0];
+		return fibArray;
+	} else if (n === 2) {
+		const fibArray = [0, 1];
+		return fibArray;
+	} else {
+		let fib1 = fibsRec(n - 1);
+		let fib2 = fibsRec(n - 2);
+
+		fib1.push(fib1[fib1.length - 1] + fib2[fib2.length - 1]);
+		return fib1;
+	}
+}
+
+//console.log(fibs(8)); // Should return [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsRec(8));
